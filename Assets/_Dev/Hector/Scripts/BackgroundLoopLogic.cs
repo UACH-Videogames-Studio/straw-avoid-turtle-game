15b525a4d055e93b9e9bcb84f5e7d38ea779d09c
HeccielMedrano <146401143+HeccielMedrano@users.noreply.github.com>
2024-08-31 16:16:49 -0600

    Add: Evenrything, actually

--- Assets/_Dev/Hector/Scripts/BackgroundLoopLogic.cs
+++ Assets/_Dev/Hector/Scripts/BackgroundLoopLogic.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BackgroundLoopLogic : MonoBehaviour
 {
+    public UnityEvent screenPassed;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private bool shouldContinueLooping = true;
     [SerializeField] protected float bgHeightUnits = 9;
@@ -26,6 +28,11 @@ public class BackgroundLoopLogic : MonoBehaviour
         }
     }
 
+    public void SetShouldContinueLooping(bool value)
+    {
+        shouldContinueLooping = value;
+    }
+
     private void LoopBackground()
     {
         float cameraCurrentPosition = mainCamera.transform.position.y;
@@ -35,6 +42,7 @@ public class BackgroundLoopLogic : MonoBehaviour
         {
             previousLoopThreshold += bgHeightUnits;
             transform.position += new Vector3(0, bgHeightUnits, 0);
+            screenPassed.Invoke();
         }
     }
 }
