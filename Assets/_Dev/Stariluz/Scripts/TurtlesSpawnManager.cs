6ccc3a75bd0528610d3b3f55afe6e67c0b68f18c
Adora <im.stariluz@gmail.com>
2024-09-01 16:25:18 -0600

    Despawn turtles

--- Assets/_Dev/Stariluz/Scripts/TurtlesSpawnManager.cs
+++ Assets/_Dev/Stariluz/Scripts/TurtlesSpawnManager.cs
@@ -13,27 +13,31 @@ public class TurtlesSpawnManager : MonoBehaviour
     [SerializeField] protected float minWaitTime = 0.5f;
     [SerializeField] protected float maxWaitTime = 2f;
     protected float waitTime = 0;
+    protected bool allowSpawn = true;
 
     void Start()
     {
     }
     void Update()
     {
-        waitTime-=Time.deltaTime;
-        if (waitTime <= 0)
+        if (allowSpawn)
         {
-            TurtlesSpawner origin = GetRandomSpawner();
-            Vector3 position=GetRandomSpawnPosition(origin.transform.position);
-            GameObject turtle = Instantiate(
-                prefab,
-                position,
-                origin.transform.rotation,
-                container
-            );
-            turtle.GetComponent<Turtle>().isMovementToRight = origin.isMovementToRight;
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0)
+            {
+                TurtlesSpawner origin = GetRandomSpawner();
+                Vector3 position = GetRandomSpawnPosition(origin.transform.position);
+                GameObject turtle = Instantiate(
+                    prefab,
+                    position,
+                    origin.transform.rotation,
+                    container
+                );
+                turtle.GetComponent<Turtle>().isMovementToRight = origin.isMovementToRight;
 
-            waitTime = GetRandomWaitTime();
-            Debug.Log(("Dev | Wait time : ", waitTime));
+                waitTime = GetRandomWaitTime();
+                // Debug.Log(("Dev | Wait time : ", waitTime));
+            }
         }
     }
     float GetRandomWaitTime()
@@ -52,13 +56,18 @@ public class TurtlesSpawnManager : MonoBehaviour
         float cameraHeight = Camera.main.orthographicSize * 2f;
 
         // Calculate the range for y based on the top half of the screen
-        float minY = origin.y + cameraHeight / 2f;
-        float maxY = origin.y + cameraHeight;
+        float minY = origin.y;
+        float maxY = origin.y + cameraHeight / 2f + 1;
 
         // Generate a random y value between minY and maxY
         float randomY = Random.Range(minY, maxY);
 
         // Return the random position with the same x and z, but with the randomized y
         return new Vector3(origin.x, randomY, 0);
+    }
+
+    public void Stop()
+    {
+        allowSpawn = false;
     }
 }
--- Assets/_Dev/Stariluz/Scripts/TurtlesSpawner.cs
+++ Assets/_Dev/Stariluz/Scripts/TurtlesSpawner.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class TurtlesSpawner : MonoBehaviour
 {
-    public bool isMovementToRight=false;
+    public bool isMovementToRight = false;
     void Start()
     {
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log((other, other.CompareTag("Enemy")));
+            Destroy(other.gameObject);
+        }
     }
 }
